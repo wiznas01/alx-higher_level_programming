e5798bbede32ea1b6ef312dc86be27f4705349b9
wiznas01 <nasiruyaro31@gmail.com>
2024-01-16 16:09:34 -0800

    generate random number and assign positive or negative to the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
+lastdigit = abs(number) % 10
+if number < 0:
+    lastdigit = -(lastdigit)
+string = "Last digit of {} is {}".format(number, lastdigit)
+if lastdigit > 5:
+    print(f"{string} and is greater than 5")
+elif lastdigit == 0:
+    print(f"{string} and is 0")
+elif lastdigit < 6:
+    print(f"{string} and is less than 6 and not 0")
