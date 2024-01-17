763c9f7060a5b96ce6a3a2c875c4b5136b8f7f83
wiznas01 <nasiruyaro31@gmail.com>
2024-01-17 14:59:47 -0800

    print and Return the value of the last digit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for iterator in str:
         temp = iterator
-        if ord(temp) >= 97 and ord(temp) <=122:
+        if ord(temp) >= 97 and ord(temp) <= 122:
             temp = chr(ord(iterator) - 32)
         print("{}".format(temp), end='')
         print()
