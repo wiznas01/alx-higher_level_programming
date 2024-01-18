26758732985b7ae1d3920f2ff8984bddc4b416a9
wiznas01 <nasiruyaro31@gmail.com>
2024-01-18 07:23:19 -0800

    program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    from calculator_1 import *
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
