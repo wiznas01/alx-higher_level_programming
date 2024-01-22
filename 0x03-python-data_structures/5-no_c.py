949b1d12016eb2da05a05c8bfc7a5bf6f618fa03
wiznas01 <nasiruyaro31@gmail.com>
2024-01-22 03:52:59 -0800

    update

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char.lower() != 'c' and char.upper() != 'C':
+            new_string += char
+        return new_string
