b99e0efea0608d5d0457d2bfa8304a8d7ba40919
wiznas01 <nasiruyaro31@gmail.com>
2024-02-08 13:24:38 -0800

    create a class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """A class representing a square
+    Attributes:
+        No attributes defined for an empty square
+    Method:
+        No methods defined for an empty square
+    """
     pass
