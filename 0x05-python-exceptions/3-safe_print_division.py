966d7fac3befed5809cfb27935bebeba8ec9a16a
wiznas01 <nasiruyaro31@gmail.com>
2024-02-08 15:42:36 -0800

    divide 2 integers and print the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """
+    Divides two integers and prints the result.
+
+    Args:
+    - a (int): The numerator.
+    - b (int): The denominator.
+
+    Returns:
+    - float or None: The result of the division, or None if an exception occurs.
+    """
+    result = None
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Inside result: None")
+        return None
+    except TypeError:
+        print("Inside result: None")
+        return None
+    if result is not None:
+            print("Inside result: {}".format(result))
+            return result
