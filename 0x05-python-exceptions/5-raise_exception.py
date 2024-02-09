f5fc332ff39358b46fea534b778d96c652ef6af9
wiznas01 <nasiruyaro31@gmail.com>
2024-02-08 16:02:59 -0800

    raise an exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    Raises a type exception.
+
+    Raises:
+    - TypeError: Always raises a TypeError.
+
+    You are not allowed to import any module.
+    """
+    raise TypeError("This is a type exception")
