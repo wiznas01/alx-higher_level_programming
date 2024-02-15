ad38ecd861cd2ec21c1140601f11378c24493f09
wiznas01 <nasiruyaro31@gmail.com>
2024-02-14 18:59:56 -0800

    Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -102,3 +102,7 @@ class Rectangle:
     def __repr__(self):
         """Return a string representation of the rectangle for reproduction."""
         return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Print a message when an instance of Rectangle is deleted."""
+        print("Bye rectangle...")
