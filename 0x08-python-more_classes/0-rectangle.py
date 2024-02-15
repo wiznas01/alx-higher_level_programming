8861d4306bc8c07b5b1b3f88f9e3ee20102b13f7
wiznas01 <nasiruyaro31@gmail.com>
2024-02-14 17:04:37 -0800

    create a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,4 +3,15 @@ class Rectangle:
     """
     A class representing a rectangle.
     """
-    pass
+    def __init__(self):
+        """
+        Initializes a new instance of the Rectangle class.
+        """
+
+    def calculate_area(self):
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            float: The area of the rectangle.
+        """
