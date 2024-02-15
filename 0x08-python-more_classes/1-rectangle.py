f5184a04bcbe207f8e0f12eaa0f851a1a8378528
wiznas01 <nasiruyaro31@gmail.com>
2024-02-14 17:45:58 -0800

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 class Rectangle:
     """
     A class representing a rectangle.
@@ -9,36 +8,16 @@ class Rectangle:
         __height (int): The height of the rectangle.
     """
 
-    def __init__(self, width=0, height=0):
+    def __init__(self, height=0, width=0):
         """
         Initializes a new instance of the Rectangle class.
 
         Parameters:
-            width (int): The width of the rectangle. Default is 0.
             height (int): The height of the rectangle. Default is 0.
+            width (int): The width of the rectangle. Default is 0.
         """
-        self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        """Retrieve the width of the rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """
-        Set the width of the rectangle.
-
-        Parameters:
-            value (int): The width of the rectangle.
-
-        Raises:
-            TypeError: If the width is not an integer.
-            ValueError: If the width is less than 0.
-        """
-        self._validate_dimension(value, "width")
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
@@ -60,6 +39,26 @@ class Rectangle:
         self._validate_dimension(value, "height")
         self.__height = value
 
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Parameters:
+            value (int): The width of the rectangle.
+
+        Raises:
+            TypeError: If the width is not an integer.
+            ValueError: If the width is less than 0.
+        """
+        self._validate_dimension(value, "width")
+        self.__width = value
+
     def _validate_dimension(self, value, dimension_name):
         """
         Validate the dimension value.
@@ -76,4 +75,3 @@ class Rectangle:
             raise TypeError(f"{dimension_name} must be an integer")
         if value < 0:
             raise ValueError(f"{dimension_name} must be >= 0")
-
