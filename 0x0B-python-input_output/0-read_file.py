03ccb6664ae65c729769712cdc95055d59a26e71
wiznas01 <nasiruyaro31@gmail.com>
2024-03-06 12:48:09 -0800

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Defines a read text file"""
 
 
@@ -14,6 +15,3 @@ def read_file(filename=""):
     """
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end='')
-
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
