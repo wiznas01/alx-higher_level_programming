7e6b3b01dbecd96ff4e70d3c2cfcd5d2f1999907
wiznas01 <nasiruyaro31@gmail.com>
2024-03-06 12:16:51 -0800

    function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF-8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the text file.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
