9c51eb8d6f91e7ff84c593865873a6dd8c0c8d08
wiznas01 <nasiruyaro31@gmail.com>
2024-04-30 08:06:15 -0700

    script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,5 +4,8 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Step 2: Convert the table charset and collation
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Step 3: Convert the field charset and collation
-ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Step 3: Alter the `id` column to match the desired structure
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN id int(11) DEFAULT NULL;
+
+-- Step 4: Convert the field charset and collation
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) DEFAULT NULL;
