8dd4e380ddf2d64437e9b4b2bf2e74f92a0e1ac0
wiznas01 <nasiruyaro31@gmail.com>
2024-04-30 07:01:21 -0700

    script that lists all records with a score >= 10 in the table second_table

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists records of table by score >= 10
-SELECT score, name FROM second_table WHERE score >= 10 BY ORDER score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
