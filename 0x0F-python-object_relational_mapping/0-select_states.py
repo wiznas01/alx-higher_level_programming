818c62a6ed5036dab5b57a77648f2e24668d22dc
wiznas01 <nasiruyaro31@gmail.com>
2024-05-03 16:26:19 -0700

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that lists all states from MySQL database"""
+import sys
+import MySQLdb
+
+def list_states(username, password, database):
+    #connect to the MySQL server
+    db = MySQLdbb.connect(host='localhost', port==3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
+
+    #Execute the SQL query from query result
+    rows = cursor.fetchall()
+
+    #print the results
+    for row in rows:
+        print(row)
+
+    #close the database connection
+    db.close()
+
+#example usage
+if __name__=='__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
